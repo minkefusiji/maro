ace4bea31dd0266aaf659480f3405c385aae6533
Huoran Li <huoranli@microsoft.com>
2021-10-15 10:45:01 +0800

    Minor

--- maro/rl/policy_v2/__init__.py
+++ maro/rl/policy_v2/__init__.py
@@ -1,13 +1,13 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from .ac import DiscreteVActorCritic
+from .ac import DiscreteQActorCritic, DiscreteVActorCritic
 from .dqn import DQN, PrioritizedExperienceReplay
 from .pg import DiscretePolicyGradient
 from .policy_base import AbsPolicy, DummyPolicy, RLPolicy, RuleBasedPolicy
 
 __all__ = [
-    "DiscreteVActorCritic",
+    "DiscreteQActorCritic", "DiscreteVActorCritic",
     "DQN", "PrioritizedExperienceReplay",
     "DiscretePolicyGradient",
     "AbsPolicy", "DummyPolicy", "RLPolicy", "RuleBasedPolicy"
